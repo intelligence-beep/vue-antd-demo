d0e1d5512ea23a724ed8cd60a6146cb211ef5fee
intelligence-beep <13151581898@163.com>
2025-05-21 03:48:18 +0800

    fix: update build config to avoid CSP issues

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/vue-antd-demo/',
+  build: {
+    target: 'es2015',
+    minify: false,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue': ['vue'],
+          'antd': ['ant-design-vue']
+        }
+      }
+    }
+  }
 }) 
