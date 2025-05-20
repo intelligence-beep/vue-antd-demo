eae4f766df01aa03b43b7f897007e59441693ee2
intelligence-beep <13151581898@163.com>
2025-05-21 04:09:03 +0800

    fix: update build config and dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-antd-demo/',
-  build: {
-    target: 'es2015',
-    minify: false,
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vue': ['vue'],
-          'antd': ['ant-design-vue']
-        }
-      }
-    }
-  }
+  base: '/vue-antd-demo/'
 }) 
