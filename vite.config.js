73eb4d0d9fc8a7d123c370b6e6995261ea62ed0d
intelligence-beep <13151581898@163.com>
2025-05-21 02:58:01 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/deeplearningAI/', // 更新为您的仓库名称
+  base: '/vue-antd-demo/', // 更新为正确的仓库名称
 }) 
